2130707f066bb3af69d5ef833661856c2f888bd1
tom <tom@sapletta.com>
2019-05-04 12:37:13 +0200

    accounts, settings, projects

---
+++ components/SettingsList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="account-list" class="wrapper">
+
+    </div>
+</template>
+
+<script>
+    import {addNote, deleteNote, toggleFavorite} from '../vuex/actions'
+
+    export default {
+        vuex: {
+            getters: {
+                activeNote: state => state.activeNote
+            },
+            actions: {
+                addNote,
+                deleteNote,
+                toggleFavorite
+            }
+        }
+    }
+</script>
