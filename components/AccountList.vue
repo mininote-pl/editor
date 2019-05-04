cb0b6e0a52f5996ca163e0060dd2691f41deb2b7
tom <tom@sapletta.com>
2019-05-04 12:05:12 +0200

    js, css

--- components/AccountList.vue
+++ components/AccountList.vue
@@ -1,103 +1,100 @@
 <template>
     <div id="account-list" class="wrapper">
-<!--        https://www.templatemonster.com/blog/how-to-add-social-media-icons-website/#title2-->
+        <!--        https://www.templatemonster.com/blog/how-to-add-social-media-icons-website/#title2-->
 
-<!--        <div class="wrapper">-->
-<!--            <ul class="social-icons icon-circle list-unstyled list-inline">-->
-<!--                <li><a href="#"><i class="fa fa-adn"></i></a> </li>-->
-<!--                <li><a href="#"><i class="fa fa-android"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-apple"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-bitbucket"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-bitbucket-square"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-bitcoin"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-css3"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-dribbble"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-dropbox"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-facebook"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-facebook-square"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-flickr"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-foursquare"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-github"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-github-alt"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-github-square"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-google-plus"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-google-plus-square"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-html5"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-instagram"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-linkedin"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-linkedin-square"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-linux"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-maxcdn"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-pagelines"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-pinterest"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-pinterest-square"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-renren"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-skype"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-stack-exchange"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-stack-overflow"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-trello"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-tumblr"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-tumblr-square"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-twitter"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-twitter-square"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-vimeo-square"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-vk"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-weibo"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-windows"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-xing"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-xing-square"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-youtube"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-youtube-play"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-youtube-square"></i></a></li>-->
-<!--            </ul>-->
+        <!--        <div class="wrapper">-->
+        <!--            <ul class="social-icons icon-circle list-unstyled list-inline">-->
+        <!--                <li><a href="#"><i class="fa fa-adn"></i></a> </li>-->
+        <!--                <li><a href="#"><i class="fa fa-android"></i></a> </li>-->
+        <!--                <li> <a href="#"><i class="fa fa-apple"></i></a> </li>-->
+        <!--                <li> <a href="#"><i class="fa fa-bitbucket"></i></a> </li>-->
+        <!--                <li> <a href="#"><i class="fa fa-bitbucket-square"></i></a> </li>-->
+        <!--                <li> <a href="#"><i class="fa fa-bitcoin"></i></a> </li>-->
+        <!--                <li> <a href="#"><i class="fa fa-css3"></i></a> </li>-->
+        <!--                <li> <a href="#"><i class="fa fa-dribbble"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-dropbox"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-facebook"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-facebook-square"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-flickr"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-foursquare"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-github"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-github-alt"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-github-square"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-google-plus"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-google-plus-square"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-html5"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-instagram"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-linkedin"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-linkedin-square"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-linux"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-maxcdn"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-pagelines"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-pinterest"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-pinterest-square"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-renren"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-skype"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-stack-exchange"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-stack-overflow"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-trello"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-tumblr"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-tumblr-square"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-twitter"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-twitter-square"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-vimeo-square"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-vk"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-weibo"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-windows"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-xing"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-xing-square"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-youtube"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-youtube-play"></i></a></li>-->
+        <!--                <li> <a href="#"><i class="fa fa-youtube-square"></i></a></li>-->
+        <!--            </ul>-->
 
-<!--            <h2>Social Icons - Hover Zoom  (.icon-zoom)</h2>-->
-<!--            <ul class="social-icons icon-circle icon-zoom list-unstyled list-inline">-->
-<!--            </ul>-->
+        <!--            <h2>Social Icons - Hover Zoom  (.icon-zoom)</h2>-->
+        <!--            <ul class="social-icons icon-circle icon-zoom list-unstyled list-inline">-->
+        <!--            </ul>-->
 
-<!--            <h2>Social Icons - Hover Rotate  (.icon-rotate)</h2>-->
-<!--            <ul class="social-icons icon-circle icon-rotate list-unstyled list-inline">-->
-<!--            </ul>-->
+        <!--            <h2>Social Icons - Hover Rotate  (.icon-rotate)</h2>-->
+        <!--            <ul class="social-icons icon-circle icon-rotate list-unstyled list-inline">-->
+        <!--            </ul>-->
 
-<!--            <hr />-->
-<!--            <h2>Social Icons - Rounded  (.icon-rounded)</h2>-->
-            <ul class="social-icons icon-rounded  list-unstyled list-inline">
-                <li> <a href="#"><i class="fa fa-github"></i></a> </li>
-                <li> <a href="#"><i class="fa fa-bitbucket"></i></a> </li>
-                <li> <a href="#"><i class="fa fa-dropbox"></i></a></li>
-            </ul>
+        <!--            <hr />-->
+        <!--            <h2>Social Icons - Rounded  (.icon-rounded)</h2>-->
 
-<!--            -->
-<!--            <h2>Social Icons - Flat  (.icon-flat)</h2>-->
-<!--            <ul class="social-icons icon-flat list-unstyled list-inline">-->
-<!--                <li><a href="#"><i class="fa fa-adn"></i></a> </li>-->
-<!--                <li><a href="#"><i class="fa fa-android"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-apple"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-bitbucket"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-bitcoin"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-css3"></i></a> </li>-->
-<!--                <li> <a href="#"><i class="fa fa-dribbble"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-dropbox"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-facebook"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-flickr"></i></a></li>-->
-<!--                <li> <a href="#"><i class="fa fa-foursquare"></i></a></li>-->
-<!--            </ul>-->
+<!--        https://fontawesome.com/how-to-use/on-the-web/setup/hosting-font-awesome-yourself-->
+        <ul class="social-icons icon-rounded  list-unstyled list-inline">
+            <li>
+                <a href="#"><i class="fas fa-user"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-github"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-bitbucket"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-dropbox"></i></a>
+            </li>
+        </ul>
+        <!--            -->
+        <!--            <h2>Social Icons - Flat  (.icon-flat)</h2>-->
+        <!--            <ul class="social-icons icon-flat list-unstyled list-inline">-->
+        <!--            </ul>-->
 
-<!--        </div>-->
-
+        <!--        </div>-->
 
 
-
-<!--        <ul class="social-networks bounce">-->
-<!--            <li><a href="https://www.linkedin.com/" class="icon-linkedin">LinkedIn</a></li>-->
-<!--            <li><a href="https://twitter.com/" class="icon-twitter">Twitter</a></li>-->
-<!--            <li><a href="https://www.facebook.com/" class="icon-facebook">Facebook</a></li>-->
-<!--            <li><a href="https://twitch.tv" class="icon-twitch">Twitch</a></li>-->
-<!--            <li><a href="https://github.com" class="icon-github">GitHub</a></li>-->
-<!--            <li><a href="https://pinterest.com" class="icon-pinterest">Pinterest</a></li>-->
-<!--            <li><a href="https://instagram.com" class="icon-instagram">Instagram</a></li>-->
-<!--            <li><a href="https://bitbucket.org" class="icon-bitbucket">Vimeo</a></li>-->
-<!--        </ul>-->
+        <!--        <ul class="social-networks bounce">-->
+        <!--            <li><a href="https://www.linkedin.com/" class="icon-linkedin">LinkedIn</a></li>-->
+        <!--            <li><a href="https://twitter.com/" class="icon-twitter">Twitter</a></li>-->
+        <!--            <li><a href="https://www.facebook.com/" class="icon-facebook">Facebook</a></li>-->
+        <!--            <li><a href="https://twitch.tv" class="icon-twitch">Twitch</a></li>-->
+        <!--            <li><a href="https://github.com" class="icon-github">GitHub</a></li>-->
+        <!--            <li><a href="https://pinterest.com" class="icon-pinterest">Pinterest</a></li>-->
+        <!--            <li><a href="https://instagram.com" class="icon-instagram">Instagram</a></li>-->
+        <!--            <li><a href="https://bitbucket.org" class="icon-bitbucket">Vimeo</a></li>-->
+        <!--        </ul>-->
 
 
     </div>
